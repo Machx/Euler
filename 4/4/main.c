05038569ce06d8c7a4546ba456557967dbfef4c3
Colin Wheeler <cocoasamurai@gmail.com>
2012-07-17 10:40:58 -0500

    Problem 4 [C]

---
+++ 4/4/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  4
+//
+//  Created by Colin Wheeler on 5/8/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+	
+	
+    return 0;
+}
+
